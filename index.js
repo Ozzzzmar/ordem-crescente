ad54560805fbe5c2df1a65b1600c37a7200d85f5
Osmar <juninhomachado579@gmail.com>
2024-06-18 08:46:20 -0500

    feat: adicionada remoção dos valores iguais

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const numeros = [77, 48, 33, 48, 27, 24, 25, 77, 77];
 
 for (let i = 0; i < numeros.length; i++){
-    console.log("No primeiro laço: " + numeros[i]);
     for (let j = i + 1; j < numeros.length; j++){
-        console.log("No segundo laço: " + numeros[j]);
         if(numeros[i] > numeros[j]){
             let aux = numeros[i];
             numeros[i] = numeros[j];
             numeros[j] = aux;
+        }
+    }
+}
+console.log(numeros);
+
+for (let i = 0; i < numeros.length; i++){
+    for (let j = i + 1; j < numeros.length; j++){
+        if(numeros[i] == numeros[j]) {
+            numeros.splice(j, 1)
+            j--;
         }
     }
 }
