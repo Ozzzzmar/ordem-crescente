4b4c6996bb4ea9896010bb3a83b2f140ea733e9b
Osmar <juninhomachado579@gmail.com>
2024-06-18 08:51:20 -0500

    feat: código otimizado

--- index.js
+++ index.js
@@ -6,15 +6,8 @@ for (let i = 0; i < numeros.length; i++){
             let aux = numeros[i];
             numeros[i] = numeros[j];
             numeros[j] = aux;
-        }
-    }
-}
-console.log(numeros);
-
-for (let i = 0; i < numeros.length; i++){
-    for (let j = i + 1; j < numeros.length; j++){
-        if(numeros[i] == numeros[j]) {
-            numeros.splice(j, 1)
+        }else if(numeros[i] == numeros[j]){
+            numeros.splice(j, 1);
             j--;
         }
     }
